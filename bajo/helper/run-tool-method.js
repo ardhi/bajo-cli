036f7cb6a02843f9eef08ec54e89188098591488
Ardhi Lukianto <ardhi@lukianto.com>
2023-12-10 18:05:46 +0700

    Updates

--- bajo/helper/run-tool-method.js
+++ bajo/helper/run-tool-method.js
@@ -2,7 +2,7 @@ import Path from 'path'
 import os from 'os'
 import net from 'net'
 
-async function runToolMethod ({ path, args = [], dir, options = {} } = {}) {
+async function runToolMethod ({ path, args = [], dir, options = {}, returnEarly } = {}) {
   const { print, importPkg, resolvePath, getConfig, generateId, log } = this.bajo.helper
   const { camelCase, map, find } = await importPkg('lodash-es')
   const [fg, select] = await importPkg('fast-glob', 'bajo-cli:@inquirer/select')
@@ -21,7 +21,8 @@ async function runToolMethod ({ path, args = [], dir, options = {} } = {}) {
   const tool = find(choices, { value: path })
   if (!tool) print.fatal('Unknown method \'%s\'', path)
   const mod = await import(resolvePath(tool.file, true))
-  await mod.default.call(this, path, args, options)
+  await mod.default.call(this, { path, args, options, returnEarly })
+  if (returnEarly) return
   if (options.demonize === '*' || (options.demonize ?? []).includes(path)) {
     log.debug('Side tool \'%s\' demonized', path)
     this.bajoCli.dsocket = os.platform() === 'win32' ? Path.join('\\\\?\\pipe', Path.resolve(config.dir.tmp), 'bajoDb', generateId()) : path.join(config.dir.tmp, 'bajoDb', generateId())
--- bajoCli/tool/package-info.js
+++ bajoCli/tool/package-info.js
@@ -1,7 +1,7 @@
 import getNpmPkgInfo from '../../bajo/helper/get-npm-pkg-info.js'
 import { input } from '@inquirer/prompts'
 
-async function packageInfo (path, args) {
+async function packageInfo ({ path, args, returnEarly }) {
   const { getConfig, importPkg, saveAsDownload, print } = this.bajo.helper
   const { prettyPrint } = this.bajoCli.helper
   const [_, stripAnsi] = await importPkg('lodash::bajo', 'strip-ansi::bajo-cli')
@@ -15,15 +15,21 @@ async function packageInfo (path, args) {
   const config = getConfig()
   const spinner = print.bora('Retrieving...').start()
   const resp = await getNpmPkgInfo(pkg)
-  if (resp.status === 404) spinner.fatal(`Unknown package '%s'. Aborted!`, pkg)
-  if (resp.status !== 200) spinner.fatal(`Can't check '%s' against npm registry. Aborted!`, pkg)
+  if (resp.status === 404) {
+    spinner.fail('Unknown package \'%s\'. Aborted!', pkg)
+    if (returnEarly) return
+  }
+  if (resp.status !== 200) {
+    spinner.fail('Can\'t check \'%s\' against npm registry. Aborted!', pkg)
+    if (returnEarly) return
+  }
   const omitted = config.full ? [] : ['readme', 'versions']
   let result = _.omit(await resp.json(), omitted)
   result = config.pretty ? (await prettyPrint(result)) : JSON.stringify(result, null, 2)
   if (config.save) {
     const file = `/${path}/${pkg}.${config.pretty ? 'txt' : 'json'}`
     const fullPath = await saveAsDownload(file, stripAnsi(result), 'bajoCli')
-    spinner.succeed(`Saved as '%s'`, fullPath, { skipSilent: true })
+    spinner.succeed('Saved as \'%s\'', fullPath, { skipSilent: true })
   } else {
     spinner.succeed('Done!')
     console.log(result)
