a4281f15a5ce6319d7bda0ea6ad792cd413ba9b7
Ardhi Lukianto <ardhi@lukianto.com>
2024-07-12 21:26:40 +0700

    Refactor

--- bajo/method/run-tool-method.js
+++ bajo/method/run-tool-method.js
@@ -2,11 +2,11 @@ import Path from 'path'
 import os from 'os'
 import net from 'net'
 
-async function runToolMethod ({ path, args = [], dir, options = {} } = {}) {
-  const { fastGlob, print, importPkg, resolvePath, getConfig, generateId } = this.bajo
-  const { camelCase, map, find } = this.bajo.lib._
+async function runToolMethod ({ path, args = [], dir, options = {}, ns } = {}) {
+  const { importPkg, resolvePath, generateId } = this.app.bajo
+  const { fastGlob } = this.app.bajo.lib
+  const { camelCase, map, find } = this.app.bajo.lib._
   const select = await importPkg('bajoCli:@inquirer/select')
-  const config = getConfig()
 
   const choices = map(await fastGlob(resolvePath(`${dir}/**/*.js`)), f => {
     const base = f.replace(dir + '/', '').slice(0, -3)
@@ -14,21 +14,21 @@ async function runToolMethod ({ path, args = [], dir, options = {} } = {}) {
   })
   if (!path) {
     path = await select({
-      message: print.__('Please select a method:'),
+      message: this.print.write('Please select a method:'),
       pageSize: 10,
       choices
     })
   }
   const tool = find(choices, { value: path })
-  if (!tool) print.fatal('Unknown method \'%s\'', path)
+  if (!tool) this.print.fatal('Unknown method \'%s\'', path)
   const mod = await import(resolvePath(tool.file, true))
-  await mod.default.call(this, { path, args, options })
-  if (!config.tool) return
+  await mod.default.call(this.app[ns], { path, args, options })
+  if (!this.app.bajo.config.tool) return
   if (options.demonize === '*' || (options.demonize ?? []).includes(path)) {
-    this.log.debug('Tool \'%s\' demonized', path)
-    this.bajoCli.dsocket = os.platform() === 'win32' ? Path.join('\\\\?\\pipe', Path.resolve(config.dir.tmp), 'bajoDb', generateId()) : path.join(config.dir.tmp, 'bajoDb', generateId())
-    this.bajoCli.daemon = net.createServer()
-    this.bajoCli.daemon.listen(this.bajoCli.dsocket)
+    this.log.debug('Tool \'%s\' is running background...', path)
+    this.dsocket = os.platform() === 'win32' ? Path.join('\\\\?\\pipe', Path.resolve(this.app.bajo.config.dir.tmp), this.name, generateId()) : path.join(this.app.bajo.config.dir.tmp, this.name, generateId())
+    this.daemon = net.createServer()
+    this.daemon.listen(this.bajoCli.dsocket)
   } else {
     process.kill(process.pid, 'SIGINT')
   }
