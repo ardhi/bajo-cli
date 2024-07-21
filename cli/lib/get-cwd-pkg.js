fe7b1f18f939aaf5c0edaa47b54db67ca10e2d2e
Ardhi Lukianto <ardhi@lukianto.com>
2024-07-21 19:36:04 +0700

    Change: app -> main

--- cli/lib/get-cwd-pkg.js
+++ cli/lib/get-cwd-pkg.js
@@ -6,7 +6,7 @@ import { fatal } from './translate.js'
 import fs from 'fs-extra'
 
 function getCwdPkg ({ argv, type }) {
-  const validator = type === 'app' ? isValidApp : isValidPlugin
+  const validator = type === 'main' ? isValidApp : isValidPlugin
   let cwd = resolvePath(process.cwd())
   if (!['.', './', '.\\', undefined].includes(argv.name)) cwd = getGlobalModuleDir(argv.name)
   if (!cwd) fatal('Unknown bajo %s: \'%s\'. Try \'bajo %s list\' to list all installed %s', type, argv.name, type, type)
