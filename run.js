c1bcb75140f30b6dba597fb20e3871f40ca488ac
Ardhi Lukianto <ardhi.lukianto@gmail.com>
2025-11-25 08:10:19 +0700

    Updates

--- run.js
+++ run.js
@@ -8,7 +8,8 @@ const lang = getLang()
 
 const y = yargs(process.argv.slice(2))
 if (lang) y.locale(lang)
-y.scriptName('bajo')
+
+  y.scriptName('bajo')
   .usage(__('Usage: $0 <command> [options]'))
   .version().alias('version', 'v')
   .command(commands)
